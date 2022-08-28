d5a57816802bc079545d41d9e42b9e91519ae66c
Gloriousjoy <gloriousfab99@gmail.com>
2022-08-28 16:56:10 +0300

    commit tree a83dca149cc11d008ab92eed4719dc45437c1d89

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //   iterate from the beginning of the string to the middle of the string
+  //   compare the letter we're iterating over to the corresponding letter at the end of the string
+  //     if the letters don't match, return false
+
+  // if we reach the middle, and all the letters match, return true
+  for (let i = 0; i < word.length / 2; i++) {
+
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+  return true;
 }
+  /* 
+    Add your pseudocode here
+  */
 
-/* 
-  Add your pseudocode here
-*/
+  /*
+    Add written explanation of your solution here
+  */
 
-/*
-  Add written explanation of your solution here
-*/
+  // You can run `node index.js` to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    console.log("");
 
-  console.log("");
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("mom"));
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+    console.log("");
 
-module.exports = isPalindrome;
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("abba"));
+
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("a"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("hi"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+
+  module.exports = isPalindrome;
